e26326fbef521bb7ee74439f1ee578c36a7699e6
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-08-13 11:51:57 +0530

    ADD : custom scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,8 +95,18 @@ const Home: NextPage = () => {
         <span className={styles.speed}>{navigatorSpeed} Mbps</span>
         <h3>Navigator API</h3>
         <footer className={styles.footer}>
-          Made with ❤️ | &copy; <a href="https://ashish-portfolio-ofc.vercel.app/" target="_blank">ASHISH</a> | <a href="https://github.com/dev-AshishRanjan/AetherNex" target="_blank">Github</a>
-      </footer>
+          Made with ❤️ | &copy;{" "}
+          <a href="https://ashish-portfolio-ofc.vercel.app/" target="_blank">
+            ASHISH
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://github.com/dev-AshishRanjan/AetherNex"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
       </section>
       <span className={styles.designSpeed}>{navigatorSpeed}</span>
     </main>
